538e9e86daa217eea1448cfd230dad0c85543147
Mohan Shanmugarajah <s404853@emirates.com>
2015-11-20 13:26:26 +0400

    initial template files

---
+++ JenkinsDemoDotNetApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinsDemoDotNetApp.Startup))]
+namespace JenkinsDemoDotNetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
